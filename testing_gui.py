a0bbe9d768195662b4f891f953b076c58956836f
Dant86 <vedantdpathak@iCloud.com>
2018-06-21 23:27:50 -0500

    wrapped it in a class

--- testing_gui.py
+++ testing_gui.py
@@ -5,7 +5,6 @@ import calendar
 #The below libraries are for `loadfont`
 from ctypes import windll, byref, create_unicode_buffer, create_string_buffer
 
-
 FR_PRIVATE  = 0x10
 FR_NOT_ENUM = 0x20
 def loadfont(fontpath, private=True, enumerable=False):
@@ -21,43 +20,56 @@ def loadfont(fontpath, private=True, enumerable=False):
 	numFontsAdded = AddFontResourceEx(byref(pathbuf), flags, 0)
 	return bool(numFontsAdded)
 
-time = ""
-dotw = ""
-date = ""
-def date_loop():
-	global toggle_colon
-	global dotw
-	global time
-	global date
-	currday = datetime.datetime.today()
-	dotw_ = calendar.day_name[currday.weekday()] + ","
-	date_ = currday.strftime("%B %d, %Y")
-	time_ = currday.strftime("%I:%M %p")
-	if time_ != time:
-		time = time_
-		clock.config(text=time)
-	if dotw_ != dotw:
-		dotw = dotw_
-		day.config(text=dotw)
-	if date_ != date:
-		date = date_
-		fulldate.config(text=date)
-	day.after(1000, date_loop)
+class ClockModule:
 
+	def __init__(self, root):
+		loadfont("fonts/Montserrat-Thin.ttf")
+		montserrat_big = Font(family="Montserrat Thin", size=72)
+		montserrat_small = Font(family="Montserrat Thin", size=48)
+
+		self.root = root
+		self.day = Label(root, font=montserrat_big, fg="white", bg="black", padx=5)
+		self.day.grid(row=0, sticky="w")
+		self.fulldate = Label(root, font=montserrat_small, fg="white", bg="black", padx=15)
+		self.fulldate.grid(row=1, column=0, sticky="w")
+		self.clock = Label(root, font=montserrat_small, fg="white", bg="black", padx=15)
+		self.clock.grid(row=2, column=0, sticky="w")
+
+		self.time = ""
+		self.dotw = ""
+		self.date = ""
+		self.loop()
+
+	def loop(self):
+		currday = datetime.datetime.today()
+		dotw = calendar.day_name[currday.weekday()] + ","
+		date = currday.strftime("%B %d, %Y")
+		time = currday.strftime("%I:%M %p")
+		if self.time != time:
+			self.time = time
+			self.clock.config(text=time)
+		if self.dotw != dotw:
+			self.dotw = dotw
+			self.day.config(text=dotw)
+		if self.date != date:
+			self.date = date
+			self.fulldate.config(text=date)
+		self.day.after(1000, self.loop)
+
+# Setting up the main "root"
+# of the hierarchy
 root = Tk()
 root.attributes('-fullscreen', True)
 root.bind('<Escape>',lambda e: root.destroy())
 root.configure(background="black")
 
-loadfont("fonts/Montserrat-Thin.ttf")
-montserrat_big = Font(family="Montserrat Thin", size=72)
-montserrat_small = Font(family="Montserrat Thin", size=48)
-day = Label(root, font=montserrat_big, fg="white", bg="black", padx=5)
-day.grid(row=0, sticky="w")
-fulldate = Label(root, font=montserrat_small, fg="white", bg="black", padx=15)
-fulldate.grid(row=1, column=0, sticky="w")
-clock = Label(root, font=montserrat_small, fg="white", bg="black", padx=15)
-clock.grid(row=2, column=0, sticky="w")
+# The "root" will contain several containers.
+# Each container will house a module.
+# For test purposes, I'm using "pack" alignment,
+# but we will use "grid" alignment soon.
+container = Frame(root, bg="black")
+container.pack(side="left", fill="both")
+clock = ClockModule(container)
 
-date_loop()
+# Runs the GUI
 root.mainloop()
